b1071ef574a82306c03834131a88e4e3c05d5bf3
Marko <marek.gazo7@gmail.com>
2020-04-08 20:57:33 +0200

    Spring Initializr

---
+++ src/main/java/com/Markococktails/Markococtail/MarkococtailApplication.java
@@ -0,0 +1,13 @@
+package com.Markococktails.Markococtail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkococtailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkococtailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Markococktails/Markococtail/MarkococtailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Markococktails.Markococtail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkococtailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
